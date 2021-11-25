5101c5583555dcfb4a2d6ac7b88f7db3884c2ad5
Jonathan Radford <jonathanradford@live.co.uk>
2021-11-25 16:17:28 +0000

    Added Project JS

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-function deleteMessages() {
+const project1 = document.getElementById("Project-Text1")
+const project2 = document.getElementById("Project-Text2")
+const project3 = document.getElementById("Project-Text3")
+const project4 = document.getElementById("Project-Text4")
+
+
+project1.addEventListener("mouseover", function() {
+  project1.innerHTML = `<div class="project-info"><p>Adventure in Codeshire!</p>
+                          <p>Choose your own adventure in Adventure in Codeshire!</p></div>`
+  setTimeout(function() {
+    project1.innerHTML = `<p>Adventure in Codeshire!</p>`
+  }, 2000);
+}, false)
+
+project2.addEventListener("mouseover", function() {
+  project2.innerHTML = `<div class="project-info"><p>Calculator</p>
+                          <p>A calculator created during week three of the _Nology Course</div>`
+  setTimeout(function() {
+    project2.innerHTML = `<p>Calculator</p>`
+  }, 2000);
+}, false)
+
+/*function deleteMessages() {
   if (window.sessionStorage.getItem("messages")) {
     window.sessionStorage.removeItem("messages");
   }
@@ -62,4 +84,4 @@ function renderMessages() {
 
 }
 
-renderMessages();
+renderMessages();*/
