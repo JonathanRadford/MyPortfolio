9f99b798e5b887b1064059d16b1adb5ff40120c9
Jonathan Radford <jonathanradford@live.co.uk>
2022-02-02 13:11:26 +0000

    added JS

--- index.js
+++ index.js
@@ -17,70 +17,45 @@ const changeClass = () => {
   }
 };
 
-/*function deleteMessages() {
-  if (window.sessionStorage.getItem("messages")) {
-    window.sessionStorage.removeItem("messages");
-  }
-  renderMessages();
-}
-
-function submitMessage(event) {
-  event.preventDefault();
-  const email = document.getElementById("email").value;
-  const fullName = document.getElementById("fullName").value;
-  const feedbackType = document.getElementById("feedbackType").value;
-  const comment = document.getElementById("comment").value;
-
-  const messageObject = {
-    email,
-    fullName,
-    feedbackType,
-    comment
+// Detect request animation frame
+var scroll =
+  window.requestAnimationFrame ||
+  // IE Fallback
+  function (callback) {
+    window.setTimeout(callback, 1000 / 60);
   };
+var elementsToShow = document.querySelectorAll(".show-on-scroll");
 
-  let currentMessages = [];
+function loop() {
+  Array.prototype.forEach.call(elementsToShow, function (element) {
+    if (isElementInViewport(element)) {
+      element.classList.add("is-visible");
+    } else {
+      element.classList.remove("is-visible");
+    }
+  });
 
-  if (window.sessionStorage.getItem("messages")) {
-    currentMessages =
-      JSON.parse(
-        window.sessionStorage.getItem("messages")
-      );
+  scroll(loop);
+}
+
+// Call the loop for the first time
+loop();
+
+// Helper function from: http://stackoverflow.com/a/7557433/274826
+function isElementInViewport(el) {
+  // special bonus for those using jQuery
+  if (typeof jQuery === "function" && el instanceof jQuery) {
+    el = el[0];
   }
-
-  currentMessages.push(messageObject);
-
-  window.sessionStorage.setItem(
-    "messages",
-    JSON.stringify(currentMessages)
+  var rect = el.getBoundingClientRect();
+  return (
+    (rect.top <= 0 && rect.bottom >= 0) ||
+    (rect.bottom >=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.top <=
+        (window.innerHeight || document.documentElement.clientHeight)) ||
+    (rect.top >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight))
   );
-  renderMessages();
 }
-
-function renderMessages() {
-  let currentMessages = [];
-
-  if (window.sessionStorage.getItem("messages")) {
-    currentMessages =
-      JSON.parse(
-        window.sessionStorage.getItem("messages")
-      );
-  }
-
-  let listItems = [];
-  for (let i = 0; i < currentMessages.length; i++) {
-    let listItem = "";
-    const currentMessage = currentMessages[i];
-    listItem += `<dt>${currentMessage.fullName} - ${currentMessage.email}</dt>`;
-    listItem += `<dd>${currentMessage.feedbackType}: ${currentMessage.comment}</dd>`;
-    listItem += `<br />`;
-    listItems.push(listItem);
-  }
-
-  let descList = document.getElementById("currentMessages");
-  descList.innerHTML = listItems.join("");
-
-}
-
-renderMessages();*/
-
-//My name is Jonathan and I chose to join the _nology course because from the outset they seemed the perfect fit for myself and my future. I have been fortunate enough to be able to perform professionally as a singer for over 5 years, whereby I was able to tour Europe multiple times, giving me life experiences I'll always treasure. However, it was touring that made me realise that I was spending far too much time away from my wife and family and I wanted to take the next step in my life, which for me was to move into a passion of mine and that was technology and coding. As an avid gamer I run a Podcast dedicated to the Video Games industry, and this combined with the creative and technical aspects of coding genuinely resonate with me, and combine my love of the technology with creativity. As someone with ADHD and Autism, it also an open space to people who aren't neurotypical, which I greatly appreciate. I realise that there are many things I need to learn, but I love learning, and this is a path I'm planning on taking for a very long time.
